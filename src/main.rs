b2021d0259da0ecb28da48736b23544f39cc76c7
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-11-22 13:11:23 +0000

    use .clone() instead of .to_owned() everywhere

--- src/main.rs
+++ src/main.rs
@@ -27,19 +27,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         if record.iter().any(|f| f.is_empty()) {
             continue;
         }
-        let spender = &record[1];
-        let spendees: Vec<&str> = record[2].split(',').map(|s| s.trim()).collect();
+        let spender = record[1].to_string();
+        let spendees: Vec<String> = record[2].split(',').map(|s| s.trim().to_string()).collect();
         let amt = record[4].parse::<f64>().unwrap() / spendees.len() as f64;
-        names.insert(spender.to_owned());
+        names.insert(spender.clone());
         for spendee in spendees.iter().filter(|s| **s != spender) {
-            names.insert(spendee.to_string());
-            owes.entry((spendee.to_string(), spender.to_owned()))
+            names.insert(spendee.clone());
+            owes.entry((spendee.clone(), spender.clone()))
                 .and_modify(|v| *v += amt)
                 .or_insert(amt);
         }
     }
     for pair in names.iter().combinations(2) {
-        let Some((ower, owee, amt)) = resolve_owe(&owes, &pair[0], &pair[1]) else {
+        let Some((ower, owee, amt)) = resolve_owe(&owes, pair[0].clone(), pair[1].clone()) else {
             continue;
         };
         println!("{ower} owes {owee} {:.2}", amt);
@@ -47,19 +47,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn resolve_owe(owes: &Owes, person1: &str, person2: &str) -> Option<(String, String, f64)> {
-    let amt_1_to_2 = owes.get(&(person1.to_owned(), person2.to_owned()));
-    let amt_2_to_1 = owes.get(&(person2.to_owned(), person1.to_owned()));
+fn resolve_owe(owes: &Owes, person1: String, person2: String) -> Option<(String, String, f64)> {
+    let amt_1_to_2 = owes.get(&(person1.clone(), person2.clone()));
+    let amt_2_to_1 = owes.get(&(person2.clone(), person1.clone()));
     match (amt_1_to_2, amt_2_to_1) {
         (None, None) => None,
-        (None, Some(amt)) => Some((person2.to_owned(), person1.to_owned(), *amt)),
-        (Some(amt), None) => Some((person1.to_owned(), person2.to_owned(), *amt)),
+        (None, Some(amt)) => Some((person2.clone(), person1.clone(), *amt)),
+        (Some(amt), None) => Some((person1.clone(), person2.clone(), *amt)),
         (Some(amt_1_to_2), Some(amt_2_to_1)) => {
             let diff = amt_1_to_2 - amt_2_to_1;
             if diff > 0.0 {
-                Some((person1.to_owned(), person2.to_owned(), diff.abs()))
+                Some((person1.clone(), person2.clone(), diff.abs()))
             } else {
-                Some((person2.to_owned(), person1.to_owned(), diff.abs()))
+                Some((person2.clone(), person1.clone(), diff.abs()))
             }
         }
     }
